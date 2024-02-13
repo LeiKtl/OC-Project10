47f8de43e142598a40f0baaf9320f2dc2c0887e2
LeiKtl <leilaktl.pro@gmail.com>
2024-02-13 12:27:26 +0100

    ✅ Update slider tests

--- src/containers/Slider/index.test.js
+++ src/containers/Slider/index.test.js
@@ -41,4 +41,38 @@ describe("When slider is created", () => {
       "Oeuvre à la coopération entre le secteur public et le privé."
     );
   });
+
+  it("should create a slide for each focused events", async () => {
+    window.console.error = jest.fn();
+    api.loadData = jest.fn().mockReturnValue(data);
+    const {container} = render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+    await new Promise(res => {window.setTimeout(res, 50)});
+    const slides = container.querySelectorAll(".SlideCard");
+    expect(slides.length).toBe(3)
+    const slidesTitle = container.querySelectorAll(".SlideCard__description h3");
+    expect(slidesTitle[0].textContent).toBe("World Farming Day");
+    expect(slidesTitle[1].textContent).toBe("World economic forum");
+    expect(slidesTitle[2].textContent).toBe("World Gaming Day");
+  });
+
+  it("should update slide each 5seconds", async () => {
+    window.console.error = jest.fn();
+    api.loadData = jest.fn().mockReturnValue(data);
+    const {container} = render(
+      <DataProvider>
+        <Slider />
+      </DataProvider>
+    );
+    await new Promise(res => {window.setTimeout(res, 50)});
+    const slides = container.querySelectorAll(".SlideCard");
+    expect(slides[0]).toHaveClass("SlideCard--display");
+    expect(slides[1]).toHaveClass("SlideCard--hide");
+    await new Promise(res => {window.setTimeout(res, 5000)});
+    expect(slides[0]).toHaveClass("SlideCard--hide");
+    expect(slides[1]).toHaveClass("SlideCard--display");
+  });
 });
